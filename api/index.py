d99f7cc675270c82a26146c27ee02ad046bf9732
Marcus <xo41555558@gmail.com>
2023-04-20 19:10:34 +0900

    코드 수정3

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ from os.path import join
 
 class handler(BaseHTTPRequestHandler):
 
-    def update_channel_stats():
+    def update_channel_stats(self):
         # 채널 ID 리스트 불러오기
         with open('data/channels_renewal_202304031340.csv', 'r') as f:
             channel_id_list = f.read().splitlines()
